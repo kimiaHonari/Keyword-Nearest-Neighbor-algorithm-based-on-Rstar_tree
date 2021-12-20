fdc919e64c883bc9f28463e00acc2750a9291a3a
kimiaHonari <63830882+kimiaHonari@users.noreply.github.com>
2021-12-20 16:21:40 -0700

    Add files via upload

---
+++ src/main/java/Tree/Rsatar/nodes/RStarNode.java
@@ -0,0 +1,51 @@
+package Tree.Rsatar.nodes;
+
+
+import Tree.Rsatar.interfaces.IDtoConvertible;
+import Tree.Rsatar.interfaces.IRStarNode;
+import Tree.Rsatar.spatial.HyperRectangle;
+import Tree.Rsatar.util.Utils;
+
+import java.util.ArrayList;
+
+public abstract class RStarNode implements IDtoConvertible, IRStarNode {
+    protected long nodeId = -1;
+    protected static int _dimension;
+    protected HyperRectangle mbr;
+
+    public ArrayList<Long> childPointers;   //ids of all children = file names are derivable from this.
+
+    private Long parentId;
+
+
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    @Override
+    public void createId() {
+        if (nodeId == -1) {
+            nodeId = Utils.getRandomId();
+            if(nodeId < 0)
+                nodeId = -1 * nodeId;
+        }
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    @Override
+    public long getNodeId() {
+        createId();
+        return nodeId;
+    }
+
+    @Override
+    public void setNodeId(long nodeId1){
+        this.nodeId = nodeId1;
+    }
+
+
+}
